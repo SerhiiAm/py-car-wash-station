3e0b9852544c654be2728c00b2a1dd0ea9674251
Serhii <sergei.amvarcumov@gmail.com>
2026-01-07 19:44:17 +0100

    Add CarWashStation methods: calculate_washing_price, wash_single_car, serve_cars, rate_service

--- app/main.py
+++ app/main.py
@@ -18,4 +18,33 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
+    def calculate_washing_price(self, car: Car) -> float:
+        price = (
+        car.comfort_class
+        * (self.clean_power - car.clean_mark)
+        * self.average_rating
+        / self.distance_from_city_center
+        )
+        return round(price, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(income, 1)
+
+    def rate_service(self, new_rate: int) -> None:
+        total_score = self.average_rating * self.count_of_ratings
+        self.count_of_ratings += 1
+        self.average_rating = round((total_score + new_rate) / self.count_of_ratings, 1)
+
+
+
+
 
